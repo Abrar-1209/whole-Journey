20340023f3a7d983ac2266c56975672e5c6c12bb
Abrar Ahmed <161435825+Abrar-1209@users.noreply.github.com>
2024-02-27 15:37:50 +0000

    Create 002variables.js

---
+++ 002variables.js
@@ -0,0 +1,12 @@
+// In terms of Programming the variable is like a container which stores values and data
+// variable is given below
+var a = "Hello";
+//here var declares the variable and var is global-scoped variable
+
+// variable can be declare with three statements known as var, let and const
+var x = 12.8;
+let y = "Hello World";
+const z = 12;
+
+
+//variables can contain different types of data like number, string, and boolean etc.
