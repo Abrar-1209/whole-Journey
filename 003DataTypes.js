0318d08b01e6e5c2c90067c957f6b8369bf635c9
Abrar Ahmed <161435825+Abrar-1209@users.noreply.github.com>
2024-02-27 15:38:13 +0000

    Create 003DataTypes.js

---
+++ 003DataTypes.js
@@ -0,0 +1,13 @@
+//There are number of data types in Javascript which are given below
+/*
+1.string
+2.number
+3.boolean
+4.float
+5.bigInt
+etc
+*/
+let age = 15;
+console.log(age);
+//this code prints the variable which is either a number or a string in the upper case it is number
+//SO it will print 15
