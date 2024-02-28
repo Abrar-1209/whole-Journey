95a9b7de3a38756aa49d555c4d14932c2888847c
Abrar Ahmed <161435825+Abrar-1209@users.noreply.github.com>
2024-02-28 05:23:48 +0000

    Create 004_objects.js

---
+++ 004_objects.js
@@ -0,0 +1,12 @@
+// An object is a collection of properties and properties is stored in a key value pair
+// An object is written below
+let items = {
+    rice: "Basmati",
+    oil: "Vegetable oil",
+    handwash: "LifeBuoy",
+    milk: "Almond milk",
+    soap: 12
+}
+
+console.log(items);
+// This will prints the whole collection of items which is written above this statment
